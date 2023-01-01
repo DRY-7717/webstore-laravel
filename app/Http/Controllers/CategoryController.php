5352e138b91195eb486bd6f98ca3ad1c6633968a
DRY-7717 <wicaksanabimaarya01@gmail.com>
2023-01-02 00:39:07 +0700

    Membuat halaman detail untuk kategori

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -11,12 +12,21 @@ class CategoryController extends Controller
     public function index()
     {
         return view('pages.category', [
-            'title' => 'Webstore Laravel | Category'
+            'title' => 'Webstore Laravel | Category',
+            'categories' => Category::all(),
+            'products' => Product::latest()->paginate(32)->withQueryString()
         ]);
     }
 
     public function show(Category $category)
     {
-        return $category;
+
+        $products = Product::where('category_id', $category->id)->paginate(32)->withQueryString();
+
+        return view('pages.category', [
+            'title' => 'Webstore Laravel | Detail Category',
+            'categories' => Category::all(),
+            'products' => $products,
+        ]);
     }
 }
